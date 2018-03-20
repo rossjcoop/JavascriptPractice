8fe9f56e11876aea029e391d417b2c9429684297
Ross Cooper <rossjamescooper@gmail.com>
2018-03-19 23:39:43 -0700

    Learned match, really cool

--- main.js
+++ main.js
@@ -523,3 +523,12 @@ function minMaxLengthAverage(arr) {
 }
 
 console.log(minMaxLengthAverage([5, 1899, 4443, 2230549, 9, 29]))
+
+
+//Create a function that takes a string and returns the number (count) of vowels contained within it.
+
+function countVowels(str) {
+  return str.match(/[aeiou]/ig).length
+}
+
+console.log(countVowels("Where in the world is Carmen San Diego?"))
