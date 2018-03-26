63224823ef527ce0f68c8d8a62fc03ae34490064
Ross Cooper <rossjamescooper@gmail.com>
2018-03-26 04:28:01 -0700

    easy one

--- main.js
+++ main.js
@@ -603,4 +603,26 @@ function highLow(str) {
 // return `${Math.max(...nums)} ${Math.min(...nums)}`; //with the template literals, works nicely.
 
 
+
 console.log(highLow("15 256 56 34 1"))
+
+
+//Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa!
+
+//Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+
+function timeForMilkAndCookies(date) {
+	let month = date.getMonth()
+	let day = date.getDay()
+
+	if(month = 11 && day = 24) {
+		return true
+	} else {
+		return false
+	}  
+}
+
+//return date.getMonth() === 11 && date.getDate() === 24; so fucking simple. Damn it. It will either be true or false. Hot beans.
+
+console.log(timeForMilkAndCookies(new Date( 2013, 11, 24 )))
+
