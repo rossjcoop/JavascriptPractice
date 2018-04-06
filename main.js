6c9f679fdc153904bd3a9f9d56a088c397d33a26
Ross Cooper <rossjamescooper@gmail.com>
2018-04-06 10:52:47 -0700

    getting there

--- main.js
+++ main.js
@@ -682,6 +682,8 @@ function mean(arr) {
 //Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
 
 function removeDups(arr) {
+	let newArr = []
+	arr.filter(i => newArr.filter(j => j == i ? newArr.push(i))
   
 }
 
