4ed85e93c534ff1e7577dc34e387629bd04b8397
Ross Cooper <rossjamescooper@gmail.com>
2019-01-24 14:50:05 -0800

    Completed another function

--- main.js
+++ main.js
@@ -919,5 +919,14 @@ function calculateExponent(num, exp) {
 
 console.log(calculateExponent(3, 2))
 
+//Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+
+// function comp(str1, str2) {
+
+	
+// }
+
+const comp = (str1, str2) => str1.split('').length == str2.split('').length ? true : false;
 
 
+console.log(comp('Ross', 'Cross'))
